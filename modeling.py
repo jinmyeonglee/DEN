206b2f017103c830921a346176c9bdbb84dc5f8d
Gyuri <misspyun1@gmail.com>
2019-11-09 00:33:30 +0900

    To see model state dict

--- modeling.py
+++ modeling.py
@@ -49,9 +49,7 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs, device, sa
                 # forward
                 with torch.set_grad_enabled(phase == 'train'):
                     # Get model outputs and calculate loss
-                    print("Input Shape : ", inputs.shape)
                     outputs = model(inputs)
-                    print("Output Shape : ", outputs.shape)
 
                     # backward + optimize only if in training phase
                     if phase == 'train':
@@ -90,6 +88,9 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs, device, sa
         time_elapsed = time.time() - since_epoch
         logger.info('Epoch complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
     
+        for param_tensor in model.state_dict():
+            print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+            
         # update saved losses
         if epoch % 2 == 0:
             pickle.dump(train_loss_history, open(os.path.join(save_dir, train_loss_pkl), 'wb'))
