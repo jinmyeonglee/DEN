a00accf250b36e658354d67f0e243721722d58f7
jinmyeonglee <jinious1111@naver.com>
2019-11-11 14:03:15 +0900

    predict

--- predict.py
+++ predict.py
@@ -2,6 +2,10 @@ import torch
 import argparse
 from den import DEN
 from torchvision.utils import save_image
+from PIL import Image
+import torchvision.transforms.functional as TF
+
+
 
 parser = argparse.ArgumentParser()
 
@@ -10,10 +14,14 @@ parser.add_argument("--image", type=str, required=True)
 parser.add_argument("--model_path", type=str, required=True)
 args = parser.parse_args()
 
-img = args.image
+
+image = Image.open(args.image)
+x = TF.to_tensor(image)
+
 den_wts = args.model_path
+
 den = DEN(den_wts)
-depth = den(img)
+depth = den(x)
 save_image(depth, "/root/DEN/images/depth_img" + args.model_path.split("/")[-1])
 
 
