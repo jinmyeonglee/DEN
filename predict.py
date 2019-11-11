615458eeb47e4c254cfb0e1f41e79829fb56013f
jinmyeonglee <jinious1111@naver.com>
2019-11-11 14:00:19 +0900

    predict

--- predict.py
+++ predict.py
@@ -1,7 +1,21 @@
 import torch
+import argparse
 from den import DEN
+from torchvision.utils import save_image
 
-img = ""
-den_wts = './models/den_gen2_v2_soomth_2/124_model.pt'
+parser = argparse.ArgumentParser()
+
+print("PyTorch Version: ",torch.__version__)
+parser.add_argument("--image", type=str, required=True)
+parser.add_argument("--model_path", type=str, required=True)
+args = parser.parse_args()
+
+img = args.image
+den_wts = args.model_path
 den = DEN(den_wts)
 depth = den(img)
+save_image(depth, "/root/DEN/images/depth_img" + args.model_path.split("/")[-1])
+
+
+
+
