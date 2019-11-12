428e78e72278939e1187e0bcd64126a4ea5d4189
jinmyeonglee <jinious1111@naver.com>
2019-11-12 19:41:43 +0900

    fix errors

--- predict.py
+++ predict.py
@@ -36,12 +36,13 @@ class FDCPredictor:
             image = pickle.load(f_img)
         nyu_dict = {'image': image, 'depth': image}
         cropped = self.transform(nyu_dict)['stacked_images']
-
         cropped.unsqueeze(0)
-        return self.fdc(cropped)[0]
+        bsz, ncrops, c, h, w = cropped.size()
+        return self.fdc(cropped.view(-1,c,h,w))[0]
     
     def save(self, img, des_path):
         save_image(img, "/root/DEN/images/depth_img/" + des_path)
+        print("/root/DEN/images/depth_img/" + des_path + "saved!")
 
 if __name__ == "__main__":
     args = parser.parse_args()
